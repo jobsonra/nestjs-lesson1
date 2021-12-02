b8f005e8f8e423a6b82838c7717cfa16d51f95ae
sonjob <jobson.ra@outlook.com>
2021-12-02 09:07:37 -0300

    refactor: altered structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { join } from 'path/posix';
 import { SequelizeModule } from '@nestjs/sequelize';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { OrdersModule } from './orders/orders.module';
 import { Order } from './orders/entities/order.entity';
 
@@ -16,7 +14,7 @@ import { Order } from './orders/entities/order.entity';
       autoLoadModels: true,
     }),
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
