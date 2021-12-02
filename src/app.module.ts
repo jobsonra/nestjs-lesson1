ac55c63ace72c20e893f015e19759cda0cdf87ef
sonjob <jobson.ra@outlook.com>
2021-12-01 23:37:39 -0300

    build: table and database connection created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
+import { join } from 'path/posix';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OrdersModule } from './orders/orders.module';
+import { Order } from './orders/entities/order.entity';
 
 @Module({
-  imports: [OrdersModule],
+  imports: [
+    OrdersModule,
+    SequelizeModule.forRoot({
+      dialect: 'sqlite',
+      host: join(__dirname, 'database.sqlite'),
+      models: [Order],
+      autoLoadModels: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/orders/entities/order.entity.ts
+++ src/orders/entities/order.entity.ts
@@ -1,1 +1,29 @@
-export class Order {}
+import {
+  Model,
+  Column,
+  DataType,
+  PrimaryKey,
+  Table,
+} from 'sequelize-typescript';
+
+export enum OrderStatus {
+  Approved = 'approved',
+  Pending = 'pending',
+}
+
+@Table({
+  tableName: 'orders',
+  createdAt: 'created_at',
+  updatedAt: 'updated_at',
+})
+export class Order extends Model {
+  @PrimaryKey
+  @Column({ type: DataType.UUID, defaultValue: DataType.UUIDV4 })
+  id: string;
+
+  @Column({ type: DataType.DECIMAL(10, 2), allowNull: false })
+  amount: number;
+
+  @Column({ allowNull: false, defaultValue: OrderStatus.Pending })
+  status: OrderStatus;
+}
